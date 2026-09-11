45546d4383114cecdec2154b939b37d9167bb51d
fmk2cjdnbp-eng <fmk2cjdnbp@privaterelay.appleid.com>
2026-09-12 08:12:02 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,1 @@
-const C="16ways-v2";self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(["./","./index.html","./manifest.webmanifest"]))));self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const C="16ways-final-v1";self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(["./","./index.html","./manifest.webmanifest"]))));
